fee12894749136be2c1b73b068cdf9025e97e26d
Marja <castanedamarjaQgmail.com>
2022-01-05 17:30:22 -0600

    repositorio con CV

---
+++ javaScript-efectos/index.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal( );
+
+sr.reveal('.secciones', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px',
+
+})
+
+
+sr.reveal('.parte-contacto', {
+    duration: 4000,
+    origin: 'bottom',
+
+})
---
+++ javaScript-formulario/index.js
@@ -0,0 +1,12 @@
+const open = document.getElementById ('open');
+const modalContainer = document.getElementById ('modalContainer');
+const close = document.getElementById ('close');
+
+open.addEventListener ('click', () =>{
+    modalContainer.classList.add('show');
+});
+
+close.addEventListener ('click', () =>{
+    modalContainer.classList.remove('show')
+});
+
